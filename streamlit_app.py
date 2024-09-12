88c85bf20668941ef1bd00b8713c8c4998805a03
Jesly Joji <133549580+Jesly-Joji@users.noreply.github.com>
2024-09-12 23:05:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,8 +27,9 @@ def process_and_annotate_video(video_path, output_path='output_video.mp4'):
     
     if not cap.isOpened():
         return []
+    
+    fourcc = cv2.VideoWriter_fourcc(*'H264')
 
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Use MP4V codec
     out = cv2.VideoWriter(output_path, fourcc, 20.0, (int(cap.get(3)), int(cap.get(4))))
 
     frames = []
