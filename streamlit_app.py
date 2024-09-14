3438842557f3d681c7f59d7ee4f93e7718f11079
Jesly Joji <133549580+Jesly-Joji@users.noreply.github.com>
2024-09-14 16:26:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,8 @@ def process_and_annotate_video(video_path, output_path='output_video.mp4'):
         st.error("Error opening video file.")
         return []
     
-    fourcc = cv2.VideoWriter_fourcc('M','J','P','G')
+ 
+    fourcc = cv.VideoWriter_fourcc(*’avc1')
     out = cv2.VideoWriter(output_path, fourcc, 20.0, (int(cap.get(3)), int(cap.get(4))))
     
     frames = []
